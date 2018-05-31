1237a17f06cabccd07627a684970e575f1523965
mlizking <keeratikinggg@gmail.com>
2018-05-31 22:33:39 +0700

    edit login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,31 +1,25 @@
 <template>
-  <v-layout column>
+  <v-layout row wrap>
     <v-flex xs12 class="text-xs-center" mt-5>
       <h3>Sign In</h3>
     </v-flex>
-    <v-flex xs12 sm6 offset-sm3 mt-3>
+    <v-flex xs12 class="text-xs-center">
       <form>
-        <v-layout column>
-          <v-flex>
             <v-text-field
               name="email"
               label="Email"
               id="email"
               type="email"
               required></v-text-field>
-          </v-flex>
-          <v-flex>
+         
             <v-text-field
               name="password"
               label="Password"
               id="password"
               type="password"
               required></v-text-field>
-          </v-flex>
-          <v-flex class="text-xs-center" mt-5>
-            <v-btn primary type="submit">Sign In</v-btn>
-          </v-flex>
-        </v-layout>
+          
+            <v-btn primary type="submit">Sign In</v-btn> 
       </form>
     </v-flex>
   </v-layout>
